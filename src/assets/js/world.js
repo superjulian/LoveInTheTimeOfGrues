af9cb382cb24eff429007dfa27a44ad5e3e01c65
JC <john.theman.connor@gmail.com>
2016-07-30 17:30:35 -0400

    added map stuff

--- src/assets/js/world.js
+++ src/assets/js/world.js
@@ -5,8 +5,45 @@ function init() {
     console.debug("init world");
 }
 
+function fill(item, board, tileset) {
+    let x_range = item.max_x - item.min_x,
+        y_range = item.max_y - item.min_y;
+    for (let i = 0; i < item.density; i++) {
+        var x = Math.floor(Math.random()*x_range) + item.min_x,
+            y = Math.floor(Math.random()*y_range) + item.min_y,
+            newitem = {"type" : item.fill, "x" : x, "y" : y};
+        create(newitem, board, tileset);
+    }
+}
+
+function create(item, board, tileset) {
+    var tiles = tileset[item.type];
+    var index = Math.floor(Math.random()*tiles.length);
+    var sprite = new createjs.Sprite(tiles[index]);
+    sprite.x = item.x;
+    sprite.y = item.y;
+    board.addChild(sprite);
+    board.update();
+}
+
+function load(path, board, tileset) {
+    var preload = new createjs.LoadQueue();
+    preload.addEventListener("fileload", function(event) {
+        console.log(event);
+        event.result.scenery.forEach(function(item) {
+            if (item.type === "fill") {
+                fill(item, board, tileset);
+            } else {
+                create(item, board, tileset);
+            }
+        });
+    });
+    preload.loadFile(path);
+}
+
 window.World = {
-    init: init
+    init: init,
+    load: load
 };
 
 })();
