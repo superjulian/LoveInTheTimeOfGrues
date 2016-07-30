b4716c556b65e87b90922c830b770c354aaa7fe4
JC <john.theman.connor@gmail.com>
2016-07-30 12:43:21 -0400

    Added basic structure.

---
+++ src/Makefile
@@ -0,0 +1,3 @@
+run:
+	python3 -m http.server
+
---
+++ src/assets/js/world.js
@@ -0,0 +1,12 @@
+(function() {
+"use strict";
+
+function init() {
+    console.debug("init world");
+}
+
+window.World = {
+    init: init
+};
+
+})();
