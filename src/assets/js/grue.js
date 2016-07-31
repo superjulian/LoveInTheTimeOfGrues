170a3040a3e6c867f1d61de8063c3cfb0e14ce2f
Julian Meltzer <jmeltzer@oberlin.edu>
2016-07-31 11:33:54 -0400

    Monster merge with animations

--- src/assets/js/grue.js
+++ src/assets/js/grue.js
@@ -8,87 +8,128 @@ var inGulp=false;
 var xCo=0;
 var yCo=0;
 var facing="";
+var idle=true;
 function init(board) {
     console.log("grue");
     grueData= {
-	images: ["assets/img/circ-brown.png", "assets/img/circ-blue.png","assets/img/circ-green.png","assets/img/circ-red.png"],
-    	frames: {width:64, height:64 },
-	animations : {
-	    idle: 0,
-            walkLeft: 1,
-	    walkRight: 2,
-	    walkUp: 3,
-	    walkDown: 2,
-        }
+	images: [
+            "assets/img/Grue-left1.png",
+            "assets/img/Grue-left2.png",
+            "assets/img/Grue-right1.png",
+            "assets/img/Grue-right2.png",
+            "assets/img/Grue-towards1.png",
+            "assets/img/Grue-towards2.png",
+            "assets/img/Grue-away1.png",
+            "assets/img/Grue-away2.png"
+        ],
+    	frames: { width:64, height:64, count: 8 },
+        animations : {
+            walkLeft: { frames: [0, 1] },
+            walkRight: { frames: [2, 3] },
+            walkDown: { frames: [4, 5] },
+            walkUp: { frames: [6, 7] },
+
+            idleLeft: { frames: [0] },
+            idleRight: { frames: [2] },
+            idleDown: { frames: [4] },
+            idleUp: { frames: [6] }
+        },
+        framerate: 5
     }
     grueSpriteSheet = new createjs.SpriteSheet(grueData);
-    grueSprite = new createjs.Sprite(grueSpriteSheet, "idle");
+    grueSprite = new createjs.Sprite(grueSpriteSheet, "idleDown");
     board.addChild(grueSprite);
 }
 
 function tick (){
 	if (inGulp===false){
-		if (Keyboard.state[Keyboard.LEFT]){
-			xCo=-1;
-			facing="LEFT";
-			grueSprite.gotoAndPlay("walkLeft");
+                if (Keyboard.state[Keyboard.SPACE]){
+                        gulp();
+                        grueSprite.gotoAndPlay("idle"+facing);
+                        idle=true;
+                }
+                else if (Keyboard.state[Keyboard.LEFT]){
+                        xCo=-1;
+                        if (facing!="Left" || idle){
+			        facing="Left";
+                                grueSprite.gotoAndPlay("walkLeft");
+                                idle=false;
+                        }
 		}
 		else if (Keyboard.state[Keyboard.RIGHT]){
-			facing="RIGHT";
-			xCo=1;
-			grueSprite.gotoAndPlay("walkRight");
+                        xCo=1;
+                        if (facing!="Right"|| idle){
+			        grueSprite.gotoAndPlay("walkRight");
+                                facing="Right";
+                                idle=false;
+                        }
 		}
-		if (Keyboard.state[Keyboard.UP]){
-			facing="UP";
-			yCo=-1;
-			grueSprite.gotoAndPlay("walkUp");
-		}if (Keyboard.state[Keyboard.DOWN]){
-			facing="DOWN";
-			yCo=1;
-			grueSprite.gotoAndPlay("walkDown");
+		else if (Keyboard.state[Keyboard.UP]){ 
+                        yCo=-1;
+                        if (facing!="Up"|| idle){
+                                facing="Up"; 
+			        grueSprite.gotoAndPlay("walkUp");
+                                idle=false;
+                        }
+		}else if (Keyboard.state[Keyboard.DOWN]){
+                        yCo=1;
+                        if (facing!="Down" || idle){
+                                facing="Down";
+                                grueSprite.gotoAndPlay("walkDown");
+                                idle=false;
+                        }
 		}
-		if (Keyboard.state[Keyboard.SPACE]){
-			gulp();
-		}
-		createjs.Tween.get(grueSprite).to({ x: grueSprite.x + (5 * xCo), y: grueSprite.y+(5 * yCo)  }, 5, createjs.Ease.linear).call(function() {xCo=0; yCo=0});	
+                else {
+                   grueSprite.gotoAndPlay("idle"+facing);
+                   idle=true;
+                }
+		createjs.Tween.get(grueSprite).to({ x: grueSprite.x + (7 * xCo), y: grueSprite.y+(7 * yCo)  }, 3, createjs.Ease.linear).call(function() {xCo=0; yCo=0});	
 	}
 }
+
 function gulp(){
 	inGulp=true;
-	var num=40;
-	var deltaX;
+	var num=20;
+        var deltaX;
 	var deltaY;
-	if (facing==="RIGHT"){
-		deltaX=[num, num];
-		deltaY=[-20, 20];
+	if (facing==="Right"){
+		deltaX=[2*num, 2*num];
+		deltaY=[-num, num];
 	}
-	else if (facing==="LEFT"){
-		deltaX=[-num, -num];
-		deltaY=[-1*(num/2), num/2];
+	else if (facing==="Left"){
+		deltaX=[-2*num, -2*num];
+		deltaY=[-num, num];
 	}
-	else if (facing==="UP"){
-		deltaX=[num/2, -num/2];
-		deltaY=[-num, -num];
+	else if (facing==="Up"){
+		deltaX=[num, -num];
+		deltaY=[-2*num, -2*num];
 	}
-	else if (facing==="DOWN"){
-		deltaX=[num/2, -num/2];
-		deltaY=[num, num];
+	else if (facing==="Down"){
+		deltaX=[num, -num];
+		deltaY=[2*num, 2*num];
 	}
 	else{
 		inGulp=false;
 		return;
 	}
-	console.log(grueSprite.y);
-	createjs.Tween.get(grueSprite)
-		.to({ x: grueSprite.x + deltaX[0], y: grueSprite.y + deltaY[0]  }, 200, createjs.Ease.getPowInOut(2))
-		.to( { x: grueSprite.x + deltaX[1], y: grueSprite.y + deltaY[1] }, 200, createjs.Ease.getPowInOut(2))
+        deltaX[0]=deltaX[0]+grueSprite.x;
+        deltaX[1]=deltaX[1]+deltaX[0];
+	deltaY[0]=deltaY[0]+grueSprite.y;
+        deltaY[1]=deltaY[1]+deltaY[0];
+        createjs.Tween.get(grueSprite)
+		.to({ x: deltaX[0], y: deltaY[0]  }, 200, createjs.Ease.getPowInOut(2))
+		.to( { x: deltaX[1], y: deltaY[1] }, 200, createjs.Ease.getPowInOut(2))
 		.call(function() {xCo=0; yCo=0; inGulp=false});
-
-	console.log(grueSprite.y);
 }
 window.Grue = {
 	init: init,
-	tick: tick
+	tick: tick,
+	//move: grueMove,
+        show: function(x, y, board) {
+                grueSprite.x = x;
+                grueSprite.y = y;
+                board.addChild(grueSprite);
+        }
 }
 
 })();
--- src/assets/js/world.js
+++ src/assets/js/world.js
@@ -1,40 +1,97 @@
 (function() {
 "use strict";
 
-function init(board) {
-    Grue.init(board);
+var board = null;
+
+const tileset = {
+    tree: [
+        new createjs.SpriteSheet({ images: ["/assets/img/Tree1.png"], frames: { width: 128, height: 192 }}),
+        new createjs.SpriteSheet({ images: ["/assets/img/Tree2.png"], frames: { width: 128, height: 192 }}),
+        new createjs.SpriteSheet({ images: ["/assets/img/Tree3.png"], frames: { width: 128, height: 192 }})
+    ],
+    grass: [
+        new createjs.SpriteSheet({ images: ["/assets/img/grass1.png"], frames: { width: 64, height: 64 }}),
+        new createjs.SpriteSheet({ images: ["/assets/img/grass2.png"], frames: { width: 64, height: 64 }}),
+    ],
+    ground: [
+        new createjs.SpriteSheet({ images: ["/assets/img/grass-blank.png"], frames: { width: 64, height: 64 }}),
+    ]
+};
+
+function sort_y(a, b) {
+    if (a.y > b.y) return 1;
+    if (a.y <= b.y) return -1;
+    return 0;
+};
+
+function init(stage) {
+    Grue.init(stage);
+    board = stage;
 }
 
-function fill(item, board, tileset) {
-    let x_range = item.max_x - item.min_x,
-        y_range = item.max_y - item.min_y;
-    for (let i = 0; i < item.density; i++) {
-        var x = Math.floor(Math.random()*x_range) + item.min_x,
-            y = Math.floor(Math.random()*y_range) + item.min_y,
-            newitem = {"type" : item.fill, "x" : x, "y" : y};
-        create(newitem, board, tileset);
+function fill(item, tileset) {
+    var newitems = [];
+    var tiles = tileset[item.fill];
+
+    if (item.density === 1) {
+        var cx = 2048 / tiles[0]._frameWidth,
+            cy = 1024 / tiles[0]._frameHeight;
+
+        for (let i = 0; i < cx; i++) {
+            for (let j = 0; j < cx; j++) {
+                let index = Math.floor(Math.random()*tiles.length),
+                    sprite = new createjs.Sprite(tiles[index]);
+                sprite.x = i * tiles[0]._frameWidth;
+                sprite.y = j * tiles[0]._frameHeight;
+                window.foo = sprite;
+                newitems.push(sprite);
+            }
+        }
+    } else {
+        for (let i = item.min_x; i < item.max_x; i += item.bound) {
+            for (let j = item.min_y; j < item.max_y; j += item.bound) {
+                let n = Math.abs(noise.simplex2(i / item.bound, j / item.bound));
+                console.log(n);
+                if (n < item.density) {
+                    var index = Math.floor(Math.random()*tiles.length),
+                        sprite = new createjs.Sprite(tiles[index]);
+                    sprite.x = i;
+                    sprite.y = j;
+                    sprite.shadow = new createjs.Shadow("rgba(0,0,0,.9)", 10, 10, 100);
+                    window.foo = sprite;
+                    newitems.push(sprite);
+                }
+            }
+        }
     }
+    newitems.sort(sort_y);
+    board.addChild.apply(board, newitems);
 }
 
-function create(item, board, tileset) {
+function create(item, tileset, at) {
     var tiles = tileset[item.type];
     var index = Math.floor(Math.random()*tiles.length);
     var sprite = new createjs.Sprite(tiles[index]);
     sprite.x = item.x;
     sprite.y = item.y;
-    board.addChild(sprite);
+    if (at) {
+        board.addChildAt(sprite, at);
+    } else {
+        board.addChild(sprite);
+    }
     board.update();
 }
 
-function load(path, board, tileset) {
+function load(path) {
     var preload = new createjs.LoadQueue();
     preload.addEventListener("fileload", function(event) {
-        console.log(event);
         event.result.scenery.forEach(function(item) {
-            if (item.type === "fill") {
-                fill(item, board, tileset);
+            if (item.type === "grue") {
+                Grue.show(item.x, item.y, board);
+            } else if (item.type === "fill") {
+                fill(item, tileset);
             } else {
-                create(item, board, tileset);
+                create(item, tileset);
             }
         });
     });
