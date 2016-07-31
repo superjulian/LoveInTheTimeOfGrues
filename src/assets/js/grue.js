54413159a8715c665b8f9bce2f3b288590781e13
Julian Meltzer <jmeltzer@oberlin.edu>
2016-07-31 16:05:38 -0400

    added eating animations

--- src/assets/js/grue.js
+++ src/assets/js/grue.js
@@ -20,10 +20,29 @@ function init(board) {
             "assets/img/Grue-towards1.png",
             "assets/img/Grue-towards2.png",
             "assets/img/Grue-away1.png",
-            "assets/img/Grue-away2.png"
+            "assets/img/Grue-away2.png",
+            "assets/img/Grue-left-eating.png",
+            "assets/img/Grue-right-eating.png",
+            "assets/img/Grue-towards-eating.png",
+            "assets/img/Grue-away-eating.png",
         ],
-    	frames: { width:64, height:64, count: 8 },
-        animations : {
+    frames: [ 
+            [0, 0, 64, 64, 0],
+            [0, 0, 64, 64, 1],
+            [0, 0, 64, 64, 2],
+            [0, 0, 64, 64, 3],
+            [0, 0, 64, 64, 4],
+            [0, 0, 64, 64, 5],
+            [0, 0, 64, 64, 6],
+            [0, 0, 64, 64, 7],
+
+            [0, 0, 128, 128, 8],
+            [0, 0, 128, 128, 9],
+            [0, 0, 128, 128, 10],
+            [0, 0, 128, 128, 11],
+            //width:64, height:64, count: 12 
+    ],
+    animations : {
             walkLeft: { frames: [0, 1] },
             walkRight: { frames: [2, 3] },
             walkDown: { frames: [4, 5] },
@@ -32,7 +51,12 @@ function init(board) {
             idleLeft: { frames: [0] },
             idleRight: { frames: [2] },
             idleDown: { frames: [4] },
-            idleUp: { frames: [6] }
+            idleUp: { frames: [6] },
+
+            eatLeft: { frames: [8] },
+            eatRight: { frames: [9] },
+            eatDown: { frames: [10] },
+            eatUp: { frames: [11] }
         },
         framerate: 5
     }
@@ -46,7 +70,6 @@ function tick (){
 	if (inGulp===false){
                 if (Keyboard.state[Keyboard.SPACE]){
                         gulp();
-                        grueSprite.gotoAndPlay("idle"+facing);
                         idle=true;
                 }
                 else if (Keyboard.state[Keyboard.LEFT]){
@@ -92,37 +115,51 @@ function tick (){
 
 function gulp(){
 	inGulp=true;
-	var num=20;
+        var num=30;
+        var rotation=0;
         var deltaX;
 	var deltaY;
 	if (facing==="Right"){
-		deltaX=[2*num, 2*num];
-		deltaY=[-num, num];
+		deltaX=[4*num, 0];
+                deltaY=[-4*num, 4*num];
+                rotation=90;
 	}
 	else if (facing==="Left"){
-		deltaX=[-2*num, -2*num];
+		deltaX=[-4*num, 0];
 		deltaY=[-num, num];
+                rotation=-90;
 	}
 	else if (facing==="Up"){
-		deltaX=[num, -num];
+		deltaX=[0, 0];
 		deltaY=[-2*num, -2*num];
+                grueSprite.gotoAndPlay("eat"+facing);
 	}
 	else if (facing==="Down"){
-		deltaX=[num, -num];
+		deltaX=[0, 0];
 		deltaY=[2*num, 2*num];
+                grueSprite.gotoAndPlay("eat"+facing);
 	}
 	else{
 		inGulp=false;
 		return;
-	}
+        }
+
+        grueSprite.rotation=-rotation/3;
         deltaX[0]=deltaX[0]+grueSprite.x;
         deltaX[1]=deltaX[1]+deltaX[0];
 	deltaY[0]=deltaY[0]+grueSprite.y;
         deltaY[1]=deltaY[1]+deltaY[0];
         createjs.Tween.get(grueSprite)
-		.to({ x: deltaX[0], y: deltaY[0]  }, 200, createjs.Ease.getPowInOut(2))
-		.to( { x: deltaX[1], y: deltaY[1] }, 200, createjs.Ease.getPowInOut(2))
-		.call(function() {xCo=0; yCo=0; inGulp=false});
+                .to({ x: deltaX[0], y: deltaY[0], rotation: rotation  }, 250, createjs.Ease.getPowInOut(2))
+                .call(function() {grueSprite.gotoAndPlay("eat"+facing)})
+		.to( { x: deltaX[1], y: deltaY[1]  }, 150, createjs.Ease.getPowInOut(2))
+                .call(function() {
+                        xCo=0; 
+                        yCo=0; 
+                        grueSprite.gotoAndPlay("idle"+facing);
+                        createjs.Tween.get(grueSprite).to( {rotation: 0}, 20, createjs.Ease.getPowInOut(2));
+                        inGulp=false});
+                
 }
 window.Grue = {
     init: init,
